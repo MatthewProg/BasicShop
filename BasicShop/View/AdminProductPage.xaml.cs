c9fd54ba85d6365c3667f06eaae4a51c5fc99975
VegetaTheKing <mat.ciaglo@gmail.com>
2020-05-30 16:55:05 +0200

    Finish AdminProductPage
    Done:
    - AdminProductPage mechanics

--- BasicShop/View/AdminProductPage.xaml.cs
+++ BasicShop/View/AdminProductPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,56 @@ namespace BasicShop.View
     /// </summary>
     public partial class AdminProductPage : Page
     {
+        CollectionViewSource viewSource;
+        shopEntities ctx = new shopEntities();
+
         public AdminProductPage()
         {
             InitializeComponent();
+            viewSource = ((CollectionViewSource)(FindResource("productViewSource")));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadScreenProcess(() =>
+            {
+                ctx.product.Load();
+            }, () =>
+            {
+                viewSource.Source = ctx.product.Local;
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            product p = new product();
+            ctx.product.Add(p);
+            viewSource.View.MoveCurrentTo(p);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            product p = viewSource.View.CurrentItem as product;
+            ctx.product.Remove(p);
+            viewSource.View.Refresh();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ctx.SaveChanges();
+            viewSource.View.Refresh();
+        }
+
+        private async void LoadScreenProcess(Action action, Action cont = null)
+        {
+            loading.Visibility = Visibility.Visible;
+
+            await Task.Factory.StartNew(action);
+
+            if (cont != null)
+                cont();
+
+            loading.Visibility = Visibility.Collapsed;
         }
     }
 }
