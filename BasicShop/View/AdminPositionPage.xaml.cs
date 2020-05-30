5c95f01dfccd27eeba405b15f61d0ecdc3476f96
VegetaTheKing <mat.ciaglo@gmail.com>
2020-05-30 16:10:52 +0200

    Finish AdminPositionPage
    Done:
    - AdminPositionPage mechanics

--- BasicShop/View/AdminPositionPage.xaml.cs
+++ BasicShop/View/AdminPositionPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,40 @@ namespace BasicShop.View
     /// </summary>
     public partial class AdminPositionPage : Page
     {
+        CollectionViewSource viewSource;
+        shopEntities ctx = new shopEntities();
+
         public AdminPositionPage()
         {
             InitializeComponent();
+            viewSource = ((CollectionViewSource)(FindResource("positionViewSource")));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ctx.position.Load();
+            viewSource.Source = ctx.position.Local;
+            loading.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            position p = new position();
+            ctx.position.Add(p);
+            viewSource.View.MoveCurrentTo(p);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            position p = viewSource.View.CurrentItem as position;
+            ctx.position.Remove(p);
+            viewSource.View.Refresh();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ctx.SaveChanges();
+            viewSource.View.Refresh();
         }
     }
 }
