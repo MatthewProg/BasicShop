5417102a487a7eb3af4a34a6c3f3e30db4873af5
VegetaTheKing <mat.ciaglo@gmail.com>
2020-05-30 18:07:13 +0200

    Version 1.0 Release
    Added:
    - app icon
    
    Done:
    - encrypted App.config file
    - better window control button colors
    
    Updated:
    - information page
    - AssemblyInfo
    - build settings

--- BasicShop/Properties/AssemblyInfo.cs
+++ BasicShop/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BasicShop")]
+[assembly: AssemblyTitle("Basic Shop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mateusz Ciaglo")]
 [assembly: AssemblyProduct("BasicShop")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©Mateusz Ciaglo 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("pl-PL")]
