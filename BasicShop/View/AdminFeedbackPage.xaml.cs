8acaeebe1554768e5317fe32ef1019173bf761f7
VegetaTheKing <mat.ciaglo@gmail.com>
2020-05-30 17:20:41 +0200

    Finish AdminFeedbackPage
    Done:
    - AdminFeedbackPage mechanics

--- BasicShop/View/AdminFeedbackPage.xaml.cs
+++ BasicShop/View/AdminFeedbackPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,56 @@ namespace BasicShop.View
     /// </summary>
     public partial class AdminFeedbackPage : Page
     {
+        CollectionViewSource viewSource;
+        shopEntities ctx = new shopEntities();
+
         public AdminFeedbackPage()
         {
             InitializeComponent();
+            viewSource = ((CollectionViewSource)(FindResource("feedbackViewSource")));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadScreenProcess(() =>
+            {
+                ctx.feedback.Load();
+            }, () =>
+            {
+                viewSource.Source = ctx.feedback.Local;
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            feedback f = new feedback();
+            ctx.feedback.Add(f);
+            viewSource.View.MoveCurrentTo(f);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            feedback f = viewSource.View.CurrentItem as feedback;
+            ctx.feedback.Remove(f);
+            viewSource.View.Refresh();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ctx.SaveChanges();
+            viewSource.View.Refresh();
+        }
+
+        private async void LoadScreenProcess(Action action, Action cont = null)
+        {
+            loading.Visibility = Visibility.Visible;
+
+            await Task.Factory.StartNew(action);
+
+            if (cont != null)
+                cont();
+
+            loading.Visibility = Visibility.Collapsed;
         }
     }
 }
