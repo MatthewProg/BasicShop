93eaa3a6cdece0bf36150481cc6797785e64b3da
VegetaTheKing <mat.ciaglo@gmail.com>
2020-05-30 16:30:53 +0200

    Finish AdminStaffPage
    Done:
    - AdminStaffPage mechanics

--- BasicShop/View/AdminStaffPage.xaml.cs
+++ BasicShop/View/AdminStaffPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,40 @@ namespace BasicShop.View
     /// </summary>
     public partial class AdminStaffPage : Page
     {
+        CollectionViewSource viewSource;
+        shopEntities ctx = new shopEntities();
+
         public AdminStaffPage()
         {
             InitializeComponent();
+            viewSource = ((CollectionViewSource)(FindResource("staffViewSource")));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ctx.staff.Load();
+            viewSource.Source = ctx.staff.Local;
+            loading.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            staff s = new staff();
+            ctx.staff.Add(s);
+            viewSource.View.MoveCurrentTo(s);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            staff s = viewSource.View.CurrentItem as staff;
+            ctx.staff.Remove(s);
+            viewSource.View.Refresh();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ctx.SaveChanges();
+            viewSource.View.Refresh();
         }
     }
 }
